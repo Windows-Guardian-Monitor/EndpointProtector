17559c9e0fe8a081586ba949caaeb7df4cc78836
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2024-07-20 13:06:53 -0300

    Remover propriedade desnecessária.

--- Common/Contracts/Models/IDiskInfo.cs
+++ Common/Contracts/Models/IDiskInfo.cs
@@ -4,7 +4,6 @@ namespace Common.Contracts.Models;
 
 public interface IDiskInfo
 {
-    public int Id { get; set; }
     Storage? AvailableSize { get; set; }
     string? DiskName { get; set; }
     string? DiskType { get; set; }
