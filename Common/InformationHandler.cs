006112f679742c0d37bea9f59442f4bfe8245333
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2024-10-16 17:48:59 -0300

    Criar modelos para enviar informações de desempenho para o servidor.

---
+++ Common/InformationHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace Common
+{
+	public static class InformationHandler
+	{
+		public static string GetUrl()
+		{
+			return "https://localhost:7102/";
+		}
+	}
+}
--- EndpointProtector/Services/Usage/CpuUsageBackgroundService.cs
+++ EndpointProtector/Services/Usage/CpuUsageBackgroundService.cs
@@ -1,34 +1,59 @@
-﻿using Common.Contracts.Providers;
-using EndpointProtector.Business.Models.Ws;
+﻿using Common;
+using Common.Contracts.Providers;
+using EndpointProtector.Business.Models.Performance;
 using System.Diagnostics;
+using System.Net.Http.Json;
 
 namespace EndpointProtector.Services.Usage
 {
-    internal class CpuUsageBackgroundService(
-        //ICpuUsageRepository cpuUsageRepository,
-        IPeriodicTimerProvider periodicTimerProvider) : BackgroundService
+	internal class CpuUsageBackgroundService(
+        IPeriodicTimerProvider periodicTimerProvider,
+        ILogger<CpuUsageBackgroundService> logger) : BackgroundService
     {
         private readonly CancellationTokenSource _tokenSource = new();
+        private HttpClient _httpClient;
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+			_httpClient = new HttpClient();
+
+			var cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
 
             var periodicTimer = periodicTimerProvider.GetServicesPeriodicTimer();
 
             do
             {
-                var dbCpuUsage = new CpuUsageInfo
+                //var dbCpuUsage = new CpuUsageInfo
+                //{
+                //    CpuUsage = cpuCounter.NextValue()
+                //};
+
+                try
                 {
-                    CpuUsage = cpuCounter.NextValue()
-                };
+					var cpuPerformanceModel = new CpuPerformanceModel()
+					{
+						CpuUsagePercentage = ConvertToPercentage(cpuCounter.NextValue()),
+					};
 
-                //cpuUsageRepository.Insert(dbCpuUsage);
+                    var url = $"{InformationHandler.GetUrl()}Performance/SendCpuPerformanceInformation";
 
-            } while (await periodicTimer.WaitForNextTickAsync() && _tokenSource.IsCancellationRequested is false);
+                    //TODO CONTINUAR DAQUI, VER SE É POSSÍVEL ENVIAR A INFO DO PROCESSADOR P API
+
+					var r = await _httpClient.PostAsJsonAsync(url, cpuPerformanceModel, cancellationToken: stoppingToken);
+				}
+                catch (Exception e)
+                {
+                    logger.LogError(e.Message);
+				}
+			} while (await periodicTimer.WaitForNextTickAsync() && _tokenSource.IsCancellationRequested is false);
         }
 
-        public override Task StopAsync(CancellationToken cancellationToken)
+		private string ConvertToPercentage(float p)
+		{
+			return $"{p}%";
+		}
+
+		public override Task StopAsync(CancellationToken cancellationToken)
         {
             _tokenSource.Cancel();
             return base.StopAsync(cancellationToken);
